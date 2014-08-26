ae02c38b698112cb518ef7d7ae3ed67775b1c0fc
Christian Danielsen & patrickdavid <student@epicodus.com>
2014-08-26 12:23:55 -0700

    update the UI with a method to add products

--- pos_ui.rb
+++ pos_ui.rb
@@ -34,4 +34,64 @@ def trippin
   sleep 2
 end
 
+def manager_menu
+  system 'clear'
+  puts 'Please choose from the following'
+  puts '[P] << Add new product'
+  puts '[C] << Add new cashier'
+  puts '[S] << View sales information'
+  puts '[I] << View product information'
+  puts '[M] << Return to main menu'
+  case gets.chomp.upcase
+  when 'P'
+    add_product
+  when 'C'
+    add_cashier
+  when 'S'
+    view_sales
+  when 'I'
+    product_info
+  when 'M'
+    welcome
+  else
+    trippin
+    manager_menu
+  end
+end
+
+def cashier_menu
+  system 'clear'
+  puts 'Please choose from the following'
+  puts '[N] << Create new sale'
+  puts '[R] << Generate reciept'
+  puts '[P] << Process refund'
+  puts '[M] << Return to main menu'
+  case gets.chomp.upcase
+  when 'N'
+    create_sale
+  when 'R'
+    reciept
+  when 'P'
+    refund
+  when 'M'
+    welcome
+  else
+    trippin
+    cashier_menu
+  end
+end
+
+def add_product
+  puts "Please enter Product name."
+  name = gets.chomp
+  puts "Please enter the price."
+  price = gets.chomp.to_f
+  new_product = Product.create({name: name, price: price})
+  puts "Your product has been added"
+  puts "Press any key to continue."
+  gets
+  manager_menu
+end
+
+
 welcome
